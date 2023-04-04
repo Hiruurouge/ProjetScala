3a47b83f7f9eabf1d175ac7bab42d94f61381dc7
Kiefferflo <kiefferflo@cy-tech.fr>
2023-04-04 16:43:43 +0200

    nouveau

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,5 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    val i = countingSort.countingSortFromFile("./src/main/assets/test.txt")
-    println(i.mkString(" "))
+    CountRDD.countingSortSpark("./src/main/assets/test.txt")
   }
 }
